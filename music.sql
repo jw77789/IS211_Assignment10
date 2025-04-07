447e3f878ea0f063f7a10393f5d4fe2e1b4f0ee2
jw77789 <JALEN.WHITE03@spsmail.cuny.edu>
2025-04-07 14:52:23 -0400

    Update music.sql

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
+-- music.sql
 
+CREATE TABLE artist (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE album (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    artist_id INTEGER NOT NULL,
+    FOREIGN KEY (artist_id) REFERENCES artist(id)
+);
+
+CREATE TABLE song (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    album_id INTEGER NOT NULL,
+    track_number INTEGER,
+    length_seconds INTEGER,
+    FOREIGN KEY (album_id) REFERENCES album(id)
+);
